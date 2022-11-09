16c2da4d2d676c890366b8f8eb386436eacf4ee7
Peter De Maeyer <peter.de.maeyer@gmail.com>
2022-11-09 07:39:05 +0100

    Removed comparator matchers

--- matchers/src/main/java/su/pernova/matchers/Matchers.java
+++ matchers/src/main/java/su/pernova/matchers/Matchers.java
@@ -25,7 +25,6 @@ import su.pernova.matchers.core.StringStartsWith;
 import su.pernova.matchers.number.BigDecimalCloseTo;
 import su.pernova.matchers.number.IsCloseTo;
 import su.pernova.matchers.number.NumberMatchers;
-import su.pernova.matchers.number.OrderingComparison;
 import su.pernova.matchers.object.HasToString;
 import su.pernova.matchers.object.IsCompatibleType;
 import su.pernova.matchers.object.IsEventFrom;
@@ -548,75 +547,6 @@ public final class Matchers {
 	 */
 	public static Matcher<BigDecimal> closeTo(BigDecimal operand, BigDecimal error) {
 		return BigDecimalCloseTo.closeTo(operand, error);
-	}
-
-	/**
-	 * Creates a matcher of {@link Comparable} object that matches when the examined object is
-	 * equal to the specified value, as reported by the <code>compareTo</code> method of the
-	 * <b>examined</b> object.
-	 * For example:
-	 * <pre>assertThat(1, comparesEqualTo(1))</pre>
-	 *
-	 * @param value the value which, when passed to the compareTo method of the examined object, should return zero
-	 */
-	public static <T extends Comparable<T>> Matcher<T> comparesEqualTo(T value) {
-		return OrderingComparison.comparesEqualTo(value);
-	}
-
-	/**
-	 * Creates a matcher of {@link Comparable} object that matches when the examined object is
-	 * greater than the specified value, as reported by the <code>compareTo</code> method of the
-	 * <b>examined</b> object.
-	 * For example:
-	 * <pre>assertThat(2, greaterThan(1))</pre>
-	 *
-	 * @param value the value which, when passed to the compareTo method of the examined object, should return greater
-	 * than zero
-	 */
-	public static <T extends Comparable<T>> Matcher<T> greaterThan(T value) {
-		return OrderingComparison.greaterThan(value);
-	}
-
-	/**
-	 * Creates a matcher of {@link Comparable} object that matches when the examined object is
-	 * greater than or equal to the specified value, as reported by the <code>compareTo</code> method
-	 * of the <b>examined</b> object.
-	 * For example:
-	 * <pre>assertThat(1, greaterThanOrEqualTo(1))</pre>
-	 *
-	 * @param value the value which, when passed to the compareTo method of the examined object, should return greater
-	 * than or equal to zero
-	 */
-	public static <T extends Comparable<T>> Matcher<T> greaterThanOrEqualTo(T value) {
-		return OrderingComparison.greaterThanOrEqualTo(value);
-	}
-
-	/**
-	 * Creates a matcher of {@link Comparable} object that matches when the examined object is
-	 * less than the specified value, as reported by the <code>compareTo</code> method of the
-	 * <b>examined</b> object.
-	 * For example:
-	 * <pre>assertThat(1, lessThan(2))</pre>
-	 *
-	 * @param value the value which, when passed to the compareTo method of the examined object, should return less
-	 * than zero
-	 */
-	public static <T extends Comparable<T>> Matcher<T> lessThan(T value) {
-		return OrderingComparison.lessThan(value);
-	}
-
-	/**
-	 * Creates a matcher of {@link Comparable} object that matches when the examined object is
-	 * less than or equal to the specified value, as reported by the <code>compareTo</code> method
-	 * of the <b>examined</b> object.
-	 * For example:
-	 * <pre>assertThat(1, lessThanOrEqualTo(1))</pre>
-	 *
-	 * @param value the value which, when passed to the compareTo method of the examined object, should return less
-	 * than or equal to zero
-	 */
-	public static <T extends Comparable<T>> Matcher<T> lessThanOrEqualTo(T value) {
-		return OrderingComparison.lessThanOrEqualTo(value);
 	}
 
 	/**
