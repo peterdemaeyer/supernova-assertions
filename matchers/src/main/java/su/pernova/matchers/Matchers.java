910c5003d81774f17fbe36c429c85cf65721ef7b
Peter De Maeyer <peter.de.maeyer@gmail.com>
2022-11-09 22:19:25 +0100

    Removed xml matchers

--- matchers/src/main/java/su/pernova/matchers/Matchers.java
+++ matchers/src/main/java/su/pernova/matchers/Matchers.java
@@ -35,7 +35,6 @@ import su.pernova.matchers.text.IsEqualCompressingWhiteSpace;
 import su.pernova.matchers.text.IsEqualIgnoringCase;
 import su.pernova.matchers.text.MatchesPattern;
 import su.pernova.matchers.text.StringContainsInOrder;
-import su.pernova.matchers.xml.HasXPath;
 
 public final class Matchers {
 
@@ -766,59 +765,6 @@ public final class Matchers {
 	 */
 	public static Matcher<EventObject> eventFrom(Object source) {
 		return IsEventFrom.eventFrom(source);
-	}
-
-	/**
-	 * Creates a matcher of {@link Node}s that matches when the examined node has a value at the
-	 * specified <code>xPath</code> that satisfies the specified <code>valueMatcher</code>.
-	 * For example:
-	 * <pre>assertThat(xml, hasXPath("/root/something[2]/cheese", equalTo("Cheddar")))</pre>
-	 *
-	 * @param xPath the target xpath
-	 * @param valueMatcher matcher for the value at the specified xpath
-	 */
-	public static Matcher<Node> hasXPath(String xPath, Matcher<String> valueMatcher) {
-		return HasXPath.hasXPath(xPath, valueMatcher);
-	}
-
-	/**
-	 * Creates a matcher of {@link Node}s that matches when the examined node has a value at the
-	 * specified <code>xPath</code>, within the specified <code>namespaceContext</code>, that satisfies
-	 * the specified <code>valueMatcher</code>.
-	 * For example:
-	 * <pre>assertThat(xml, hasXPath("/root/something[2]/cheese", myNs, equalTo("Cheddar")))</pre>
-	 *
-	 * @param xPath the target xpath
-	 * @param namespaceContext the namespace for matching nodes
-	 * @param valueMatcher matcher for the value at the specified xpath
-	 */
-	public static Matcher<Node> hasXPath(String xPath, NamespaceContext namespaceContext, Matcher<String> valueMatcher) {
-		return HasXPath.hasXPath(xPath, namespaceContext, valueMatcher);
-	}
-
-	/**
-	 * Creates a matcher of {@link Node}s that matches when the examined node contains a node
-	 * at the specified <code>xPath</code>, with any content.
-	 * For example:
-	 * <pre>assertThat(xml, hasXPath("/root/something[2]/cheese"))</pre>
-	 *
-	 * @param xPath the target xpath
-	 */
-	public static Matcher<Node> hasXPath(String xPath) {
-		return HasXPath.hasXPath(xPath);
-	}
-
-	/**
-	 * Creates a matcher of {@link Node}s that matches when the examined node contains a node
-	 * at the specified <code>xPath</code> within the specified namespace context, with any content.
-	 * For example:
-	 * <pre>assertThat(xml, hasXPath("/root/something[2]/cheese", myNs))</pre>
-	 *
-	 * @param xPath the target xpath
-	 * @param namespaceContext the namespace for matching nodes
-	 */
-	public static Matcher<Node> hasXPath(String xPath, NamespaceContext namespaceContext) {
-		return HasXPath.hasXPath(xPath, namespaceContext);
 	}
 
 	/**
