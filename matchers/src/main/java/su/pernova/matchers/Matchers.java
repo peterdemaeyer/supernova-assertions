24adfceb8876640de956220a2a4313e72e355ec4
Peter De Maeyer <peter.de.maeyer@gmail.com>
2022-11-09 23:03:25 +0100

    Removed number matchers

--- matchers/src/main/java/su/pernova/matchers/Matchers.java
+++ matchers/src/main/java/su/pernova/matchers/Matchers.java
@@ -1,12 +1,8 @@
 package su.pernova.matchers;
 
-import java.math.BigDecimal;
-
 import su.pernova.matchers.core.CoreMatchers;
 import su.pernova.matchers.core.EqualsMatcher;
 import su.pernova.matchers.core.SameAsMatcher;
-import su.pernova.matchers.number.BigDecimalCloseTo;
-import su.pernova.matchers.number.IsCloseTo;
 import su.pernova.matchers.number.NumberMatchers;
 
 public final class Matchers {
@@ -101,33 +97,6 @@ public final class Matchers {
 	 */
 	public static Matcher<Object> equalToObject(Object operand) {
 		return EqualsMatcher.equalToObject(operand);
-	}
-
-	/**
-	 * Creates a matcher of {@link Double}s that matches when an examined double is equal
-	 * to the specified <code>operand</code>, within a range of +/- <code>error</code>.
-	 * For example:
-	 * <pre>assertThat(1.03, is(closeTo(1.0, 0.03)))</pre>
-	 *
-	 * @param operand the expected value of matching doubles
-	 * @param error the delta (+/-) within which matches will be allowed
-	 */
-	public static Matcher<Double> closeTo(double operand, double error) {
-		return IsCloseTo.closeTo(operand, error);
-	}
-
-	/**
-	 * Creates a matcher of {@link BigDecimal}s that matches when an examined BigDecimal is equal
-	 * to the specified <code>operand</code>, within a range of +/- <code>error</code>. The comparison for equality
-	 * is done by BigDecimals {@link BigDecimal#compareTo(BigDecimal)} method.
-	 * For example:
-	 * <pre>assertThat(new BigDecimal("1.03"), is(closeTo(new BigDecimal("1.0"), new BigDecimal("0.03"))))</pre>
-	 *
-	 * @param operand the expected value of matching BigDecimals
-	 * @param error the delta (+/-) within which matches will be allowed
-	 */
-	public static Matcher<BigDecimal> closeTo(BigDecimal operand, BigDecimal error) {
-		return BigDecimalCloseTo.closeTo(operand, error);
 	}
 
 	/**
